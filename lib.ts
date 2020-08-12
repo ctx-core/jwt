b9ebdd8ae5e549fd96d27c217d2d9792ac537f92
Brian Takita <brian.takita@gmail.com>
2020-08-11 21:16:35 -0700

    - @ctx-core/logger

--- lib.ts
+++ lib.ts
@@ -1,7 +1,5 @@
 import { _atob } from '@ctx-core/atob'
 import { throw__bad_credentials } from '@ctx-core/error'
-import { log } from '@ctx-core/logger'
-const logPrefix = '@ctx-core/jwt/lib'
 export type Token = {
 	error?:any
 	id_token?:string
@@ -11,7 +9,6 @@ export function _token__jwt__authorization__header(authorization) {
 	return a1__authorization && a1__authorization[1]
 }
 export function validate__current__jwt(token__jwt) {
-	log(`${logPrefix}|validate__current__jwt`)
 	const exp__token__jwt = _exp__token__jwt(token__jwt)
 	const millis__exp__token__jwt = exp__token__jwt * 1000
 	if (Date.now() > millis__exp__token__jwt) {
