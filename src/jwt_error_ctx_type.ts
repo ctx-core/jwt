4aafd917e1c6fe93949202b93264d76fc18bf5ab
Brian Takita <brian.takita@gmail.com>
2021-02-24 00:37:35 -0500

    update dependencies

--- src/jwt_error_ctx_type.ts
+++ src/jwt_error_ctx_type.ts
@@ -1,4 +1,4 @@
-import type { error_ctx_type } from '@ctx-core/error'
-export interface jwt_error_ctx_type extends error_ctx_type {
+import type { error_ctx_T } from '@ctx-core/error'
+export interface jwt_error_ctx_type extends error_ctx_T {
 	jwt_token:string
 }
