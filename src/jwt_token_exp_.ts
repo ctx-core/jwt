fa11aad85157f365483ae89785abc1712374c47f
Brian Takita <brian.takita@gmail.com>
2021-06-09 01:38:38 -0400

    use optional chain

--- src/jwt_token_exp_.ts
+++ src/jwt_token_exp_.ts
@@ -1,9 +1,9 @@
 import type { nullish } from '@ctx-core/function'
-import { _atob } from '@ctx-core/atob'
+import { atob_ } from '@ctx-core/atob'
 export function jwt_token_exp_(jwt_token:string|nullish) {
-	const atob = _atob()
+	const atob = atob_()
 	const jwt_data = jwt_token && JSON.parse(atob(jwt_token.split('.')[1]))
-	return jwt_data && jwt_data.exp
+	return jwt_data?.exp
 }
 export {
 	jwt_token_exp_ as _jwt_token_exp,
